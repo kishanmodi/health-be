866d45fbb23832a169982d58d77ba138fc54917f
Kishan Modi <kishanmodi@macbookair.home>
2026-02-21 16:23:04 -0500

    updated patients model

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Patient(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    diagnosis = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Patient
+from .serializers import PatientSerializer
 
-# Create your views here.
+class PatientViewSet(viewsets.ModelViewSet):
+    serializer_class = PatientSerializer
+
+    def get_queryset(self):
+        return Patient.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
