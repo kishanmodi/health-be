ce90b517cdd45ab89c91372873a02ae47417c44d
Kishan Modi <kishanmodi@macbookair.home>
2026-02-21 16:27:23 -0500

    updated doctors model

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    hospital = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Doctor
+from .serializers import DoctorSerializer
 
-# Create your views here.
+class DoctorViewSet(viewsets.ModelViewSet):
+    serializer_class = DoctorSerializer
+
+    def get_queryset(self):
+        return Doctor.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
